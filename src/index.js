90bad89cdfe54db2cffa1a0160590b3727971817
Daniyar <kulovdaniyar77@gmail.com>
2022-02-21 11:47:18 +0600

    feat: done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+	bracketsConfig = {
+		')': '(',
+		']': '[',
+		'}': '{',
+		'|': '|',
+	};
+	const st = [];
+	for (let i = 0; i < str.length; i++) {
+		if (isClosedBracket(str[i])) {
+
+			if (bracketsConfig[str[i]] !== st.pop()) return false;
+		} else {
+			st.push(str[i]);
+
+		}
+	}
+	return st.length === 0;
+}
+function isClosedBracket(ch) {
+	return [')', ']', '}', '|'].indexOf(ch) > -1;
 }
